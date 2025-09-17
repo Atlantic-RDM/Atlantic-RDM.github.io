f571b41475ffc3ee781b941577ac0e36eb593fb3
Nick Rochlin <nick.rochlin@gmail.com>
2025-09-17 15:03:39 -0700

    build survey cleaning

--- docs/data/survey-cleaning-workshop/survey-cleaning1.R
+++ docs/data/survey-cleaning-workshop/survey-cleaning1.R
@@ -3,18 +3,28 @@
 
 library(tidyverse)
 
-js_survey <- read_csv("js-test-survey.csv")
+survey_data <- read_csv("data/survey-cleaning-workshop/social-media-survey.csv")
 
-View(js_survey)
+names(survey_data)
+
+head(survey_data)
+
+View(survey_data)
 
 # Rename columns to be friendlier
-js_survey <- js_survey |>
-  rename("year-of-study" = "Year of Study",
-         "hours-per-day" = "How many hours per day do you spend on social media?",
-         "platforms" = "Which social media platforms do you use at least once a week?",
-         "feel-connected" = "Social media makes me feel connected",
-         "feel-increase-stress" = "Social media increases my stress",
-         "feel-distracted" = "I find social media distracting from studies",
+survey_data <- survey_data |>
+  rename("year_of_study" = "Year of Study")
+         
+survey_data <- survey_data |>
+  rename("hours-per-day" = "How many hours per day do you spend on social media?") 
+
+survey_data <- survey_data |>
+  rename("platforms" = "Which social media platforms do you use at least once a week?",
+          "feel-connected" = "Social media makes me feel connected",
+          "feel-increase-stress" = "Social media increases my stress") 
+
+survey_data <- survey_data |>
+  rename( "feel-distracted" = "I find social media distracting from studies",
          "feel-improved-mood" = "Social media positively impacts my mood",
          "usage-stay-in-touch" = "Staying connected with friends/family",
          "usage-entertainment" = "Entertainment",
@@ -23,81 +33,81 @@ js_survey <- js_survey |>
          "usage-expression" = "Self-expression/creativity")
 
 # Remove capitals from column names because I'm a weirdo
-js_survey <- js_survey |>
+survey_data <- survey_data |>
   rename("timestamp" = "Timestamp",
          "name" = "Name",
-         "email" = "Email")
+         "email" = "Email",
+         "age" = "Age",
+         "gender" = "Gender")
 
 # Add an ID column
-js_survey <- js_survey |>
+survey_data <- survey_data |>
   mutate(ID = row_number())
 
 # Move ID to first position
-js_survey <- js_survey |>
+survey_data <- survey_data |>
   relocate(ID)
 
-# Save the dataset to preserve provenance
-write_csv(js_survey, "js_survey-clean-identifiers.csv")
+View(survey_data)
 
+# Save the dataset to preserve provenance
+write_csv(survey_data, "js_survey-clean-identifiers.csv")
+
+survey_data <- read_csv("data/survey-cleaning-workshop/js_survey-clean-identifiers.csv")
+
+View(survey_data)
 
 ## Step 2: Remove identifiers and unnecessary columns
 
 # Remove a single column - timestamp
-js_survey <- js_survey[, -2]
+survey_data <- survey_data |>
+  select(-timestamp)
 
 # Remove multiple columns - identifiers
-js_survey <- js_survey[, -c(2, 3)]
+survey_data <- survey_data |>
+  select(-c(Name, Email))
 
 # Save dataset as ready for processing
-write_csv(js_survey, "js_survey-clean-id.csv")
+write_csv(survey_data, "survey-data_clean-cols_no-ID")
 
+survey_data <- read_csv("data/survey-cleaning-workshop/js_survey-clean-id.csv")
 
 ## Step 3: Clean 'platforms' variable with tidy data principles
 
-# Separate each platform into its own row, by participant ID
-js_survey_long <- js_survey |>
-  separate_rows(platforms, sep = ",") |> # split responses by commas
-  mutate(platforms = trimws(platforms)) # remove leading/trailing spaces
+View(survey_data)
 
+head(survey_data$platforms)
+
+
+# Separate each platform into its own row, by participant ID
+survey_data_long <- survey_data |>
+  separate_longer_delim(cols = platforms, delim = ",") |> 
+  mutate(platforms = trimws(platforms)) 
+
+
+write_csv(survey_data, "survey_data_long.csv")
 # Take a peak
 View(js_survey_long)
 
-# Create new columns for each platform, with 1 being used, and 0 not being used at least once a week
-js_survey_wide <- js_survey_long |>
-  mutate(value = 1) |>
-  pivot_wider(
-    names_from = platforms,
-    values_from = value,
-    values_fill = 0
+survey_data_longer <- survey_data_long |>
+  pivot_longer(
+    cols = starts_with("feel-"),
+    names_to = "feel_question",
+    values_to = "feel_response"
   )
 
+View(survey_data_long)
+
+survey_data_double_long <- survey_data_longer |>
+  pivot_longer(
+  cols = starts_with("usage-"),
+  names_to = "usage_question",
+  values_to = "usage_response"
+)
+
 # Take another peak
-View(js_survey_wide)
+View(survey_data_double_long)
 
-# Move platforms to where they were in the original dataset
-
-# Move a single column, LinkedIn, to AFTER hours-per-day
-js_survey_wide <- js_survey_wide |>
-  relocate(LinkedIn, .after = `hours-per-day`)
-
-# Move a single column, Instagram, BEFORE connected
-js_survey_wide <- js_survey_wide |>
-  relocate(Instagram, .before = "feel-connected")
-
-# Move multutiple columns with one of these functions
-js_survey_wide <- js_survey_wide |>
-  relocate(Reddit, Facebook, Snapchat, TikTok, X (Twitter), .after = Instagram)
-
-# Oh dear, we've run into a program with "X (Twitter) Due to its naming.  Let's change this
-js_survey_wide <- js_survey_wide |>
-  rename("X" = "X (Twitter)")
-
-# Let's try to move those columns again, changing the name of "X (Twitter)" to "X"
-js_survey_wide <- js_survey_wide |>
-  relocate(Reddit, Facebook, Snapchat, TikTok, X, .after = Instagram)
-
-
-# Now there's one last thing we need to do.  
 
 # Save a copy of this dataset, as the cleaning is done and we can start poking around at it!
 write_csv(js_survey_wide, "js_survey_analysis.csv")
